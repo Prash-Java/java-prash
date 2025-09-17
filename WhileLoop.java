54aa9e068438d84ef4cd1636e976317b2d63cff8
Prashant Choudhary <93462306+Prash-Java@users.noreply.github.com>
2025-09-17 20:45:47 +0530

    Add files via upload

--- WhileLoop.java
+++ WhileLoop.java
@@ -2,15 +2,16 @@ public class WhileLoop {
     public static void main(String[] args) {
         int i = 1;
         while (i <= 5) {
-            System.out.print(i + " ");
+            System.out.print("Hello" + i);
             int j = 1;
             while(j<=3){
-                System.out.print(j);
+                System.out.println();
+                System.out.print("Hello" + j);
                 ++j;
             }
-            System.out.println(j);
+            System.out.println("Ending value of inner while counter " + j);
             ++i;
         }
-        System.out.println(i);
+        System.out.println("Ending value of outer while counter " + i);
     }
 }
